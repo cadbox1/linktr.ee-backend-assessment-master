59e64c988bafa76f09ab0e4e7823284cb8f0356c
Cadell Christo <git@cadell.dev>
2020-10-13 15:07:02 +0800

    setup node server

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # README
 
+## Getting Started
+
+Install Node 12.14.1 or run `nvm use` if you're using NVM.
+
+## Scripts
+
+- `npm install` - install dependencies.
+- `npm run start` - start the server on `http://localhost:3000/`.
+
---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+// import { taskRouter } from "./services/task/task-router";
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+app.use(bodyParser.json());
+
+// app.use("/task", taskRouter);
+
+// @ts-ignore
+app.get("/", async (req, res) => {
+	res.send("Hello world");
+});
+
+export default app;
